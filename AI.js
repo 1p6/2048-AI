1ff3bc77e3a8356f53a6e5e6df7b412f8e94b16d
1p6 <aaapf12@gmail.com>
2014-05-05 14:27:09 -0500

    Update AI.js

--- AI.js
+++ AI.js
@@ -4,7 +4,7 @@ var highScore = 0;
 var b = document.createElement('button');
 b.innerHTML = "Get High Score";
 b.setAttribute('onclick', 'alert(highScore);');
-document.body.appendChild(b);
+document.body.insertBefore(b, document.body.fistChild);
 
 function gameOver(){
   return document.getElementsByClassName("game-over").length > 0;
